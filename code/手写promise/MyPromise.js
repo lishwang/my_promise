1d09c4d350001f7c15bbb5104d1df3774db4bf2f
wanglish <739100735@qq.com>
2022-10-27 21:33:35 +0800

    实现promise的状态只能改变一次

--- code/手写promise/MyPromise.js
+++ code/手写promise/MyPromise.js
@@ -1,24 +1,28 @@
 
 function MyPromise (executor) {
   // 添加属性
-  this.promiseState = 'pending';
-  this.promiseResult = null;
+  this.PromiseState = 'pending';
+  this.PromiseResult = null;
   // 保存实例对象的this
   let self = this;
   // 定义 resolve 函数
   function resolve (data) {
     // 注意：函数作用域中的this默认指向函数调用者，如果没有调用者，就指向window
-    // 1、修改对象上的状态（是Promise实例对象上的一个属性 promiseState）
-    self.promiseState = 'fulfilled';
-    // 2、设置对象结果值（是Promise实例对象上的另一个属性 promiseResult）
-    self.promiseResult = data;
+    // 判断状态是否为 pending ，保证无论实例对象中调用多少次 resolve 和 reject，promise 的状态只能更改一次
+    if (self.PromiseState !== 'pending') return;
+    // 1、修改对象上的状态（是Promise实例对象上的一个属性 PromiseState）
+    self.PromiseState = 'fulfilled';
+    // 2、设置对象结果值（是Promise实例对象上的另一个属性 PromiseResult）
+    self.PromiseResult = data;
   };
   // 定义 reject 函数
   function reject (data) {
-    // 1、修改对象上的状态（是Promise实例对象上的一个属性 promiseState）
-    self.promiseState = 'rejected';
-    // 2、设置对象结果值（是Promise实例对象上的另一个属性 promiseResult）
-    self.promiseResult = data;
+    // 判断状态是否为 pending ，保证无论实例对象中调用多少次 resolve 和 reject，promise 的状态只能更改一次
+    if (self.PromiseState !== 'pending') return;
+    // 1、修改对象上的状态（是Promise实例对象上的一个属性 PromiseState）
+    self.PromiseState = 'rejected';
+    // 2、设置对象结果值（是Promise实例对象上的另一个属性 PromiseResult）
+    self.PromiseResult = data;
   };
   try {
     // 同步调用 执行器函数executor , 并传入实参 resolve 和 reject
