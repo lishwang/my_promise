5f83101f2b78bf648bb87be3977e534a12629c60
wanglish <739100735@qq.com>
2022-10-27 21:12:01 +0800

    promise搭建

--- code/手写promise/promise.js
+++ code/手写promise/promise.js
@@ -1,6 +1,11 @@
 
 function MyPromise (executor) {
-
+  // 定义 resolve 函数
+  function resolve (data) { };
+  // 定义 reject 函数
+  function reject (data) { };
+  // 同步调用 执行器函数executor , 并传入实参 resolve 和 reject
+  executor();
 }
 
 MyPromise.prototype.then = function (onResolved, onRejected) { };
